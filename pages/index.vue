92c4cbbc1ba63a6b9edb6e2a64cce3bc26a26c84
Jakkaphat <gunt@guntxjakka.me>
2022-04-25 08:10:53 +0700

    feat: add definition info

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex justify-center pt-20">
+  <div class="flex justify-center pt-20 overflow-y-scroll">
     <Modal v-if="isModalVisible" @close="closeModal" />
 
-    <div class="fixed items-top overflow-hidden text-center" v-else>
+    <div class="fixed items-top text-center" v-else>
       <div class="flex text-center justify-center">
         <h1 class="text-5xl text-center flex py-5 text-red-400">
           Wordle Answer
@@ -11,11 +11,28 @@
       <hr class="h-5" />
       <p class="flex justify-center text-center text-xl">{{ day }}</p>
       <h2 class="flex justify-center text-center mt-5 text-2xl">
-        Today's Answer:
+        <b>Today's Answer:</b>
       </h2>
       <h2 class="text-4xl mt-3 flex justify-center text-center">{{ word }}</h2>
+      <br />
+      <h2 class="text-xl mt-3 justify-center text-left ml-4">
+        <b>Pronunciation:</b> {{ pronunciation }}
+      </h2>
+      <h2 class="text-xl mt-3 justify-center text-left break-words ml-4">
+        <b>Definition:</b> <i>{{ pos }}</i> <br />
+        {{ definition }}
+      </h2>
+      <h2 class="text-lg mt-3 justify-center text-left ml-4">
+        see more
+        <a
+          :href="this.link"
+          class="text-lg underline text-blue-500 hover:text-blue-400"
+          >here</a
+        >
+      </h2>
     </div>
-    <Footer />
+    <br />
+    <Footer v-if="!isModalVisible" />
   </div>
 </template>
 
@@ -32,13 +49,28 @@ export default Vue.extend({
   data() {
     const epochDate = 1624060800000
     const today = new Date()
-    const day = Math.floor((today.getTime() - epochDate) / 86400000)
+    const day = Math.floor((today.getTime() - epochDate) / 86400000) + 1
     const word = wl.words
     return {
       isModalVisible: true,
       day: day,
       word: word[day],
+      pronunciation: '',
+      definition: '',
+      pos: '',
+      link: '',
     }
+  },
+  mounted() {
+    this.$axios
+      .$get(`https://api.dictionaryapi.dev/api/v2/entries/en/${this.word}`)
+      .then((x) => {
+        let meanings = x[0].meanings[0]
+        this.pronunciation = x[0].phonetic
+        this.definition = meanings.definitions[0].definition
+        this.pos = meanings.partOfSpeech
+        this.link = x[0].sourceUrls[0]
+      })
   },
   methods: {
     closeModal() {
