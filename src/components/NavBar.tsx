01db444e1351e770283768965c1cba1d0b8281a8
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2023-09-02 02:30:32 +0700

    feat: v2
    * move to nextjs
    
    * utilize new wordle api

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = () => {
+    return (
+        <div
+        className="
+        flex
+        justify-between
+        text-base text-black
+        bg-gray-300
+        dark:text-white dark:bg-gray-900
+        shadow-2xl
+        items-left
+        "
+    >
+        <div className="flex items-center justify-end ml-4">
+        <Link href="/" className="block px-3 py-2">Home</Link>
+        <a
+            href="https://github.com/gxjakkap/wordle-ans"
+            target="_blank"
+            className="block px-3 py-2"
+            >GitHub</a>
+        </div>
+        <button
+        id="theme-toggle"
+        type="button"
+        className="
+            text-gray-500
+            dark:text-gray-400
+            hover:bg-gray-100
+            dark:hover:bg-gray-700
+            focus:outline-none focus:ring-4 focus:ring-gray-200
+            dark:focus:ring-gray-700
+            rounded-lg
+            text-sm
+            px-5
+            py-1
+        "
+        >
+        {/* <FontAwesomeIcon icon={faMoon} className="cursor-pointer text-xl" />
+        <FontAwesomeIcon icon={faSun} className="cursor-pointer text-xl" /> */}
+        </button>
+    </div>
+    )
+}
+
+export default NavBar
