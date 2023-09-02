2e030a2c84bf8cbe126a8fc32d161ca079d0e812
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2023-09-02 14:25:22 +0700

    fix: serverless functions timed out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
@@ -9,18 +10,21 @@ import NavBar from '@/components/NavBar'
 import WDAFooter from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
+const karnak = localFont({ src: '../font/karnakcondensed.otf' })
 
 interface WdData {
    word: string,
-   day: number,
-   dictAvailable: boolean,
+   day: number
+
+}
+
+interface DictData {
    pronunciation: string,
    definition: string,
    pos: string,
    link: string,
    audioLink: string,
    audioAvailable: boolean
-
 }
 
 export const getServerSideProps: GetServerSideProps<WdData> = async() => {
@@ -33,53 +37,10 @@ export const getServerSideProps: GetServerSideProps<WdData> = async() => {
 
     else {
         const nytWordleData = await nytRes.json()
-        
-        const dictApiRes = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${nytWordleData['solution']}`)
-        let dictAvailable = true
-        if (dictApiRes.status !== 200){
-            dictAvailable = false
-        }
-        
-        const dictData = await dictApiRes.json()
-
-        const meanings = dictData[0].meanings[0]
-
-        let pronunciation = ''
-        let audioLink = ''
-        let audioAvailable = false
-
-        const phoneticnoS = dictData[0]['phonetic'] || null
-        const phoneticswithS = dictData[0]['phonetics'] || null
-
-        if (phoneticnoS && !phoneticswithS){
-            pronunciation = phoneticnoS
-        }
-        else if (!phoneticnoS || phoneticswithS) {
-            pronunciation = phoneticswithS[0]['text']
-            phoneticswithS.forEach((x: any) => {
-                if (x['audio']){
-                    audioLink = x['audio']
-                    pronunciation = x['text']
-                    audioAvailable = true
-                }
-            })
-        }
-        else {
-            // fallback block
-            console.log('fallback')
-        }
-
         return {
             props: {
                 word: nytWordleData['solution'] as string,
                 day: nytWordleData['days_since_launch'] as number,
-                dictAvailable,
-                pronunciation,
-                definition: meanings.definitions[0].definition,
-                pos: meanings.partOfSpeech,
-                link: dictData[0]['sourceUrls'][0],
-                audioLink,
-                audioAvailable,
             }
         }
     }
@@ -87,16 +48,70 @@ export const getServerSideProps: GetServerSideProps<WdData> = async() => {
     
 }
 
-export default function Home({word, day, dictAvailable, pronunciation, definition, pos, link, audioLink, audioAvailable}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home({word, day}: InferGetServerSidePropsType<typeof getServerSideProps>) {
     const [showModal, setShowModal] = useState(true)
     const [pronunAudio, setPronunAudio] = useState<HTMLAudioElement | null>(null)
+    const [dictAvailable, setDictAvailable] = useState(false)
+    const [dictData, setDictData] = useState<DictData>({
+        pronunciation: '',
+        definition: '',
+        pos: '',
+        link: '',
+        audioLink: '',
+        audioAvailable: false
+    })
+
 
     useEffect(() => {
-        setPronunAudio(new Audio(audioLink))
+        const fetchDictData = async() => {
+            const dictApiRes = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+            if (dictApiRes.status !== 200){
+                return {}
+            }
+            setDictAvailable(true)
+            const dictData = await dictApiRes.json()
+            const meanings = dictData[0].meanings[0]
+            let pronunciation = ''
+            let audioLink = ''
+            let audioAvailable = false
+
+            const phoneticnoS = dictData[0]['phonetic'] || null
+            const phoneticswithS = dictData[0]['phonetics'] || null
+
+            if (phoneticnoS && !phoneticswithS){
+                pronunciation = phoneticnoS
+            }
+            else if (!phoneticnoS || phoneticswithS) {
+                pronunciation = phoneticswithS[0]['text']
+                phoneticswithS.forEach((x: any) => {
+                    if (x['audio']){
+                        audioLink = x['audio']
+                        pronunciation = x['text']
+                        audioAvailable = true
+                    }
+                })
+            }
+            else {
+                // fallback block
+                console.log('fallback')
+            }
+            setDictData({
+                pronunciation,
+                audioLink,
+                audioAvailable,
+                definition: meanings['definitions'][0]['definition'],
+                pos: meanings['partOfSpeech'],
+                link: dictData[0]['sourceUrls'][0]
+            })
+            setPronunAudio(new Audio(audioLink))
+        }
+        fetchDictData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const playAudio = () => {
+        console.log(dictData.audioLink)
+        console.log(dictData)
         if (pronunAudio){
             pronunAudio.play()
         }
@@ -120,7 +135,7 @@ export default function Home({word, day, dictAvailable, pronunciation, definitio
         >
             <NavBar />
             <div className="flex text-center justify-center">
-                <h1 className="text-5xl text-center flex py-5 text-red-400">
+                <h1 className={`text-5xl text-center flex py-5 dark:text-white ${karnak.className}`}>
                     Wordle Answer
                 </h1>
             </div>
@@ -155,8 +170,8 @@ export default function Home({word, day, dictAvailable, pronunciation, definitio
             <br />
             {dictAvailable ? (<div className="justify-center text-center items-center">
                 <h2 className="text-xl mt-3 justify-center ml-5 dark:text-slate-300">
-                    <b>Pronunciation:</b> { pronunciation }
-                    {audioAvailable ? (<FontAwesomeIcon icon={faVolumeHigh} onClick={() => {playAudio()}} className='cursor-pointer ml-2' />) : (<></>)}
+                    <b>Pronunciation:</b> { dictData.pronunciation }
+                    {dictData.audioAvailable ? (<FontAwesomeIcon icon={faVolumeHigh} onClick={() => {playAudio()}} className='cursor-pointer ml-2' />) : (<></>)}
                 </h2>
                 <h2
                     className="
@@ -168,12 +183,12 @@ export default function Home({word, day, dictAvailable, pronunciation, definitio
                     dark:text-slate-300
                     "
                 >
-                    <b>Definition:</b> <i>{ pos }</i>
+                    <b>Definition:</b> <i>{ dictData.pos }</i>
                     <br />
-                    { definition }
+                    { dictData.definition }
                 </h2>
                 <h2 className="text-lg mt-3 justify-centerml-5 dark:text-slate-300">
-                    see more <a href={link} className="text-lg underline text-blue-500 hover:text-blue-400">here</a>
+                    see more <a href={dictData.link} className="text-lg underline text-blue-500 hover:text-blue-400">here</a>
                 </h2>
             </div>) : (<></>)}
         </div>
